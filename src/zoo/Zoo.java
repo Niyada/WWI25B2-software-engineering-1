9e1536801e9f368dc13f6cb7ccee7dcd32dc7769
Lorisegg <loris.egg06@gmail.com>
2025-10-27 13:08:57 +0100

    added new animal Sheep changed numberOfAnimals

--- src/zoo/Zoo.java
+++ src/zoo/Zoo.java
@@ -3,15 +3,17 @@ package zoo;
 import animals.Animal;
 import animals.Dog;
 import animals.Fox;
+import animals.Sheep;
 
 public class Zoo {
     public static void main(String[] args) {
-        int numberOfAnimals = 3;
+        int numberOfAnimals = 4;
         Animal[] allAnimals = new Animal[numberOfAnimals];
 
         allAnimals[0] = new Dog("Rex");
         allAnimals[1] = new Dog("Bella");
         allAnimals[2] = new Fox("Peter");
+        allAnimals[3] = new Sheep("Dolly");
 
          for (Animal currAnimal : allAnimals) {
             System.out.println("\nA new animal has arrived:");
