bef30d361fdbbe4839cc196dc0fe4f784f137bc4
oliver <edingero@gmail.com>
2025-10-30 12:51:10 +0100

    Add Bird Class Removed Conflicts by changing number of Animals

--- src/zoo/Zoo.java
+++ src/zoo/Zoo.java
@@ -6,12 +6,12 @@ import animals.Dog;
 
 public class Zoo {
     public static void main(String[] args) {
-        int numberOfAnimals = 3;
+        int numberOfAnimals = 6;
         Animal[] allAnimals = new Animal[numberOfAnimals];
 
         allAnimals[0] = new Dog("Rex");
         allAnimals[1] = new Dog("Bella");
-        allAnimals[2] = new Bird("Ralf");
+        allAnimals[5] = new Bird("Ralf");
 
          for (Animal currAnimal : allAnimals) {
             System.out.println("\nA new animal has arrived:");
