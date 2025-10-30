5ff1b03a9db885c17ca01b893d040ca2f7d28285
oliver <edingero@gmail.com>
2025-10-30 12:43:54 +0100

    Add Bird Class overwrite makeSound for Bird

--- src/zoo/Zoo.java
+++ src/zoo/Zoo.java
@@ -1,15 +1,17 @@
 package zoo;
 
 import animals.Animal;
+import animals.Bird;
 import animals.Dog;
 
 public class Zoo {
     public static void main(String[] args) {
-        int numberOfAnimals = 2;
+        int numberOfAnimals = 3;
         Animal[] allAnimals = new Animal[numberOfAnimals];
 
         allAnimals[0] = new Dog("Rex");
         allAnimals[1] = new Dog("Bella");
+        allAnimals[2] = new Bird("Ralf");
 
          for (Animal currAnimal : allAnimals) {
             System.out.println("\nA new animal has arrived:");
