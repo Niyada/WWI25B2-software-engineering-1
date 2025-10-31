7fc2b355f14d12452c9057fe060fe27f5c4b3a1e
Zoe Haas <zoehaas2004.zh@gmail.com>
2025-10-31 07:52:14 +0100

    add Duck class

--- src/zoo/Zoo.java
+++ src/zoo/Zoo.java
@@ -4,7 +4,7 @@ import animals.*;
 
 public class Zoo {
     public static void main(String[] args) {
-        int numberOfAnimals = 7;
+        int numberOfAnimals = 8;
         Animal[] allAnimals = new Animal[numberOfAnimals];
 
         allAnimals[0] = new Dog("Rex");
@@ -14,6 +14,7 @@ public class Zoo {
         allAnimals[4] = new Shark("Horst");
         allAnimals[5] = new Bird("Ralf");
         allAnimals[6] = new Cat("Klaus");
+        allAnimals[7] = new Duck("Daisy");
 
          for (Animal currAnimal : allAnimals) {
             System.out.println("\nA new animal has arrived:");
